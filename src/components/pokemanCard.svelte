cec189179981bd4b014071f137be8b5b3f040235
Haris Wijayanto <hrswjynto@gmail.com>
2021-12-28 02:50:27 +0700

    first comit

---
+++ src/components/pokemanCard.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { fade } from 'svelte/transition';
+
+	export let pokeman;
+</script>
+
+<a
+	class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center mb-4"
+	href={`/pokemon/${pokeman.id}`}
+	transition:fade
+>
+	<img class="h-40 w-40" src={pokeman.image} alt={pokeman.name} />
+	<h2 class="uppercase text-2xl">
+		{pokeman.id}. {pokeman.name}
+	</h2>
+</a>
---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>Svelte Kit About</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About The Svelte Kit Pokedex</h1>
+<p>This is a really cool Svelte Kit demo that uses the Pokemon API to build a Pokedex</p>

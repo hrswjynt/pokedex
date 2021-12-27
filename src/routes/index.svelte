c53e1e7652a62aef6217f206ef5b1c13e3331b68
Haris Wijayanto <hrswjynto@gmail.com>
2021-12-28 02:54:55 +0700

    all pokemon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
 	export async function load({ page }) {
-		const url = `https://pokeapi.co/api/v2/pokemon/?limit=150`;
+		const url = `https://pokeapi.co/api/v2/pokemon/`;
 		const res = await fetch(url);
 		const data = await res.json();
 		const loadedPokemon = data.results.map((data, index) => {
